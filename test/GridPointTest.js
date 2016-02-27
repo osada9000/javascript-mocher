fd530eedc95a61f99dfbe94269a37c264c762f7e
osada <osada9000@gmail.com>
2016-02-27 14:29:19 +0900

    Done 1-2

--- test/GridPointTest.js
+++ test/GridPointTest.js
@@ -8,7 +8,6 @@ describe('格子点', function () {
     var sut;
 
     describe('x:1, y:2を与えたとき', function () {
-
         beforeEach(function () {
             sut = new GridPoint(1, 2);
         });
@@ -24,6 +23,15 @@ describe('格子点', function () {
         it('文字列表記は (1,2) であるべき', function () {
             assert.ok(sut.getNotation() === '(1,2)');
         });
+
+        it('別の格子点(1,2)と比較して同じ座標であるべき', function () {
+            assert.ok(sut.hasSameCoordinatesWith(new GridPoint(1, 2)) === true);
+        });
+
+        it('格子点(2,2)と比較して異なる座標であるべき', function () {
+            assert.ok(sut.hasSameCoordinatesWith(new GridPoint(2, 2)) === false);
+        });
+
     });
 
 });
