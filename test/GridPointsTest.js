3bae861057d3418dc2d4a3a4a675c5e3993e2a02
osada <osada9000@gmail.com>
2016-02-27 17:01:49 +0900

    Done 4-1

--- test/GridPointsTest.js
+++ test/GridPointsTest.js
@@ -10,19 +10,20 @@ describe('格子点集合', function () {
 
     describe('格子点集合(5,7),(11,13)を与えたとき', function () {
         beforeEach(function () {
-            sut = new GridPoints(new GridPoint(5, 7), new GridPoint(11, 13));
+            var args = [new GridPoint(5, 7), new GridPoint(11, 13)];
+            sut = new GridPoints(args);
         });
 
         it('格子点集合に(5,7)が含まれているべき', function () {
-            assert.ok(sut.has(new GridPoint(5,7)) === true);
+            assert.ok(sut.has(new GridPoint(5, 7)) === true);
         });
 
         it('格子点集合に(11,13)が含まれているべき', function () {
-            assert.ok(sut.has(new GridPoint(11,13)) === true);
+            assert.ok(sut.has(new GridPoint(11, 13)) === true);
         });
 
         it('格子点集合に(2,3)が含まれていないべき', function () {
-            assert.ok(sut.has(new GridPoint(2,3)) === false);
+            assert.ok(sut.has(new GridPoint(2, 3)) === false);
         });
 
         it('格子点が隣接してない', function () {
@@ -33,7 +34,8 @@ describe('格子点集合', function () {
 
     describe('格子点集合(2,2),(2,3)を与えたとき', function () {
         beforeEach(function () {
-            sut = new GridPoints(new GridPoint(2, 2), new GridPoint(2, 3));
+            var args = [new GridPoint(2, 2), new GridPoint(2, 3)];
+            sut = new GridPoints(args);
         });
 
         it('格子点が隣接している', function () {
@@ -41,4 +43,61 @@ describe('格子点集合', function () {
         });
 
     });
+
+    describe('格子点集合(2,2),(2,3),(2,4)を与えたとき', function () {
+        beforeEach(function () {
+            var args = [
+                new GridPoint(2, 2),
+                new GridPoint(2, 3),
+                new GridPoint(2, 4)
+            ];
+            sut = new GridPoints(args);
+        });
+
+        it('格子点集合に(2,2)が含まれているべき', function () {
+            assert.ok(sut.has(new GridPoint(2,2)) === true);
+        });
+
+        it('格子点集合に(2,3)が含まれているべき', function () {
+            assert.ok(sut.has(new GridPoint(2,3)) === true);
+        });
+
+        it('格子点集合に(2,4)が含まれているべき', function () {
+            assert.ok(sut.has(new GridPoint(2, 4)) === true);
+        });
+
+        it('格子点が隣接している', function () {
+            assert.ok(sut.connected() === true);
+        });
+
+    });
+
+    describe('格子点集合(2,2),(2,3),(2,5)を与えたとき', function () {
+        beforeEach(function () {
+            var args = [
+                new GridPoint(2, 2),
+                new GridPoint(2, 3),
+                new GridPoint(2, 5)
+            ];
+            sut = new GridPoints(args);
+        });
+
+        it('格子点集合に(2,2)が含まれているべき', function () {
+            assert.ok(sut.has(new GridPoint(2,2)) === true);
+        });
+
+        it('格子点集合に(2,3)が含まれているべき', function () {
+            assert.ok(sut.has(new GridPoint(2,3)) === true);
+        });
+
+        it('格子点集合に(2,5)が含まれているべき', function () {
+            assert.ok(sut.has(new GridPoint(2, 5)) === true);
+        });
+
+        it('格子点が隣接していないべき', function () {
+            assert.ok(sut.connected() === false);
+        });
+
+    });
+
 });
